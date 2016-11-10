9c3ee89ea79f6e8298358e6f2b7728280a67c33a
Jose Deschamps <deschamps.jose001@gmail.com>
2016-11-10 18:55:55 -0500

    Project is up

---
+++ Project/Wasabi/Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ChangeToScene(int SceneToChangeTo){
+
+
+		SceneManager.LoadScene (SceneToChangeTo);
+
+
+	}
+}
