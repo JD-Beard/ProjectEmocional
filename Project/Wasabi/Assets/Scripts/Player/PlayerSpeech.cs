ef705f85a184dad6daa54a9befe2c9678ae6b1b4
Jose Deschamps <deschamps.jose001@gmail.com>
2016-11-11 21:27:27 -0500

    updated stuff

--- Project/Wasabi/Assets/Scripts/Player/PlayerSpeech.cs
+++ Project/Wasabi/Assets/Scripts/Player/PlayerSpeech.cs
@@ -7,14 +7,14 @@ public class PlayerSpeech : MonoBehaviour {
 
 	public Transform spawnPoint;
 	public ParticleSystem effect;
-	public ParticleSystem HitEffect;
-	public AudioClip[] audioClip;
+//	public ParticleSystem HitEffect;
+//	public AudioClip[] audioClip;
 	private bool isFinish = true;
-	private int randomNumber;
-	public int MyLife = 3;
-	public Image live;
-	public Image live1;
-	public Image live2;
+	//private int randomNumber;
+	//public int MyLife = 3;
+//	public Image live;
+//	public Image live1;
+//	public Image live2;
 
 
 
@@ -26,50 +26,51 @@ public class PlayerSpeech : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		randomNumber = Random.Range (0, 3);
+		//randomNumber = Random.Range (0, 3);
 
 
-		if (Input.GetMouseButtonDown (0)&& isFinish==true) {
+		if (Input.GetKeyDown(KeyCode.Joystick1Button3) && isFinish==true){
 
 
 			Instantiate (effect, spawnPoint.position, spawnPoint.rotation);
-			PlaySound (randomNumber);
-			StartCoroutine (CheckingTime ());
-			isFinish = false;
+		//	PlaySound (randomNumber);
+//			CheckingTime();
+//			isFinish = false;
 
 		}
+			}
 
 
-		if (MyLife == 2) {
+//		if (MyLife == 2) {
+//
+//			live.enabled = false;
+//
+//		}
 
-			live.enabled = false;
-
-		}
-
-		if (MyLife == 1) {
-
-			live1.enabled = false;
-
-		}
-
-		if (MyLife == 0) {
-
-			live2.enabled = false;
-
-		}
+//		if (MyLife == 1) {
+//
+//			live1.enabled = false;
+//
+//		}
+//
+//		if (MyLife == 0) {
+//
+//			live2.enabled = false;
+//
+//		}
+//	
 	
-	}
 
 
 
-	public void PlaySound(int clip){
-
-		AudioSource audio = GetComponent<AudioSource> ();
-		audio.clip = audioClip [clip];
-		audio.Play ();
-
-
-	}
+//	public void PlaySound(int clip){
+//
+//		AudioSource audio = GetComponent<AudioSource> ();
+//		audio.clip = audioClip [clip];
+//		audio.Play ();
+//
+//
+//	}
 
 
 
@@ -81,35 +82,35 @@ public class PlayerSpeech : MonoBehaviour {
 
 	}
 
-	IEnumerator GameOver(){
-
-		yield return new WaitForSeconds (1);
-		SceneManager.LoadScene ("Gameover");
-
-	}
-
-
-
-	void OnCollisionEnter(Collision other){
-
-		if (other.collider.gameObject.tag == "Club") {
-
-			MyLife--;
-			PlaySound (3);
-			Instantiate (HitEffect, transform.position, transform.rotation);
-
-			if (MyLife == 0) {
-
-				StartCoroutine (GameOver ());
+//	IEnumerator GameOver(){
+//
+//		yield return new WaitForSeconds (1);
+//		SceneManager.LoadScene ("Gameover");
+//
+//	}
 
 
 
-			}
+//	void OnCollisionEnter(Collision other){
+//
+//		if (other.collider.gameObject.tag == "Club") {
+//
+//			MyLife--;
+//			PlaySound (3);
+//			Instantiate (HitEffect, transform.position, transform.rotation);
+//
+//			if (MyLife == 0) {
+//
+//				StartCoroutine (GameOver ());
+//
+//
+//
+//			}
+//
+//		}
 
-		}
 
-
-	}
+	//}
 
 
 }
