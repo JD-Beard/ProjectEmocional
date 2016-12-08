4c751dc4c7d84d852f98289708f734dd7c0de2c7
tiffanyvlee <tiffanyvlee@gmail.com>
2016-12-08 03:48:19 -0500

    script edit

--- Project/Wasabi/Assets/Scripts/backAndForth.cs
+++ Project/Wasabi/Assets/Scripts/backAndForth.cs
@@ -17,13 +17,10 @@ public class backAndForth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// set position for the cube as an interpolation
+
 		transform.position = Vector3.Lerp(pointA.position, pointB.position, percent);
 		transform.rotation = Quaternion.Slerp (pointA.rotation, pointB.rotation, percent);
-		// for lerping a float: Mathf.Lerp ();
 
-		//percent = percent + 1/60f;
-		// Time.deltaTime is the amount of change between this frame and the last one
 		percent += Time.deltaTime / numberOfSeconds * direction;
 		if (percent > 1) {
 			direction = -1;
