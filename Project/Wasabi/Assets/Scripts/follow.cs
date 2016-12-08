af0392a65e2cd93814171761a0b315f5798fb4d8
tiffanyvlee <tiffanyvlee@gmail.com>
2016-12-08 03:33:37 -0500

    added blue moving creatures prefab
    in the models folder!

---
+++ Project/Wasabi/Assets/Scripts/follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+
+	public Transform thingToFollow;
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (thingToFollow);
+		transform.position += transform.forward * Time.deltaTime * speed;
+	
+	}
+}
